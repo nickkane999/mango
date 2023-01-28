ca16fab1a38573dabd15cf5174052ec912fada46
nickkane999 <nickkane999@gmail.com>
2023-01-28 16:51:11 -0500

    Didn't end up using ReactQueryDevtools for this, so going to disable it. Pushing to update my site with it

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+//import { ReactQueryDevtools } from "react-query/devtools";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const queryClient = new QueryClient({
@@ -25,7 +25,6 @@ root.render(
     <ApolloProvider client={client}>
       <QueryClientProvider client={queryClient}>
         <App />
-        <ReactQueryDevtools />
       </QueryClientProvider>
     </ApolloProvider>
   </React.StrictMode>
